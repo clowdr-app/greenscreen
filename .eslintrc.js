593d077792c3f46f25c0044e63ea71df8953b589
Ross Gardiner <github@public.rossng.eu>
2021-12-03 15:46:44 +0000

    chore: docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,43 @@
 /** @type import('eslint').Linter.Config */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    // jest: true,
-    // "jest/globals": true,
-  },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        // jest: true,
+        // "jest/globals": true,
     },
-    ecmaVersion: 2021,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    semi: ["warn", "always"],
-    quotes: ["error", "double", { avoidEscape: true }],
-    // "jest/no-disabled-tests": "warn",
-    // "jest/no-focused-tests": "error",
-    // "jest/no-identical-title": "error",
-    // "jest/prefer-to-have-length": "warn",
-    // "jest/valid-expect": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        args: "after-used",
-        ignoreRestSiblings: false,
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "(^_)|(.*Query$)",
-      },
-    ],
-  },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+        semi: ["warn", "always"],
+        quotes: ["error", "double", { avoidEscape: true }],
+        // "jest/no-disabled-tests": "warn",
+        // "jest/no-focused-tests": "error",
+        // "jest/no-identical-title": "error",
+        // "jest/prefer-to-have-length": "warn",
+        // "jest/valid-expect": "error",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                vars: "all",
+                args: "after-used",
+                ignoreRestSiblings: false,
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "(^_)|(.*Query$)",
+            },
+        ],
+    },
 };
--- prepare.js
+++
@@ -1,5 +0,0 @@
-const isCi =
-  process.env.CI !== undefined || process.env.NODE_ENV === "production";
-if (!isCi) {
-  require("husky").install();
-}
